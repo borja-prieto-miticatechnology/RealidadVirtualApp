67dd4bcc14ad94a906ac8b8f6ccd4385447210dc
DESKTOP-SF6FAJJ\BorjaMzl <borjalloren14@gmail.com>
2021-01-15 11:39:44 +0100

    Adaptacion textos

--- Assets/Resources/Scripts/Textos.cs
+++ Assets/Resources/Scripts/Textos.cs
@@ -20,9 +20,9 @@ public class Textos : MonoBehaviour
     public static string texto4 = "¡Genial! Continuemos.";
     public static string texto5 = "Ahora vamos a coger algunos objetos. Localiza el cubo amarillo y recogele presionando el gatillo trasero del mando";
     public static string texto6 = "¡Perfecto! ¡Ya lo tienes! Empecemos con la formación";
-    public static string texto7 = "Accede al cuadro de mandos pulsando el boton de menú y utiliza los botones para interactuar con el medio. Cuando haya terminado avance a la formación 2 a través del botón Empezar prueba.";
+    public static string texto7 = "Abre y cierra la tablet pulsando el boton de menú y utiliza los botones para interactuar con el medio. Cuando haya terminado avance a la formación 2 a través del botón Empezar prueba.";
     public static string texto8 = "¡Perfecto! Primera parte de la formación con IMMERSE completada, pulse el boton de avance a formación 2 en el cuadro de mandos";
-    public static string texto9 = "¡Atención! Ha saltado la alarma de deteccion de humos. Sigue las instrucciones que te mostraremos a continuación para llevar una correcta ejecución del plan de seguridad del edificio";
+    public static string texto9 = "¡Atención! Ha saltado la alarma de deteccion de humos. Sigue las instrucciones para llevar una correcta ejecución del plan de seguridad del edificio.";
     public static string texto10 = "Localiza la bombona de oxigeno y cogela";
     public static string texto11 = "Dirigete hacia la salida por las escaleras, las flechas te indicarán el camino";
     public static string texto12 = "¡ENHORABUENA! ¡HAS COMPLETADO CON ÉXITO LA FORMACIÓN!";
