78ac3010c670af215c65a313752b919ffc03d509
Nekroarteriaa <Nekroarteriaa@gmail.com>
2021-01-19 09:51:34 +0100

    Adding Path

---
+++ Assets/Resources/Scripts/Pathfinding/PathwayBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using PathCreation;
+using System.Collections;
+
+public class PathwayBehaviour : MonoBehaviour
+{
+    public PathCreator pathC;
+    public bool isDeleting;
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (!isDeleting)
+        {
+            isDeleting = true;
+            StartCoroutine(DeleteSegment());
+        }
+    }
+
+    IEnumerator DeleteSegment()
+    {
+        pathC.bezierPath.DeleteSegment(0);
+        yield return new WaitForSeconds(2);
+        isDeleting = false;
+    }
+}
